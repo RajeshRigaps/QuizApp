1ba9a7846f82ec4aa3a980523e7e603659500ac5
RajeshSudhaveni <rajeshsudhaveni469@gmail.com>
2025-02-06 20:34:57 +0530

    feat: Add name input and send to quiz page usin state

--- quiz-app-frontend/src/pages/Home.jsx
+++ quiz-app-frontend/src/pages/Home.jsx
@@ -1,13 +1,34 @@
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate} from "react-router-dom";
+import "../assets/styles/Home.css";
 
 const Home = () => 
     {
+        const [name, setName] = useState("Guest");
+        const navigate = useNavigate();
+        const handleStart = () => {
+            if(name === "Guest") {
+                alert("Please enter your name to start the quiz");
+            }
+            else {
+                navigate("/quiz", {state: {name}});
+            }
+        }
         return(
-        <div>
+        <div className="home-container">
+            <div className="home-content">
             <h1>Welcome to Quiz App</h1>
-            <Link to="/quiz">
-                <button>Start Now</button>
-            </Link>
+            <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="name-input"
+            />
+            
+                <button className="start-button" onClick={handleStart} >Start Now</button>
+          
+            </div>
         </div>);
     }
 export default Home
--- quiz-app-frontend/src/pages/QuizPage.jsx
+++ quiz-app-frontend/src/pages/QuizPage.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom";
+import "../assets/styles/QuizPage.css";
 const QuizPage = () => 
-    { return(
-        <div>
-            <h1>Quiz Page</h1>
+    { 
+        const location = useLocation();
+        const name = location.state.name;
+        return(
+        <div className="quiz-container">
+            <h1>Welcome, {name}</h1>
         </div>
     );
 }
