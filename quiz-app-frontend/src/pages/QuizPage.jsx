82abb7dd73eb9f8dd1a7859083846538c1790dab
RajeshSudhaveni <rajeshsudhaveni469@gmail.com>
2025-02-10 18:18:58 +0530

    feat: shuffle the question and options order

--- quiz-app-frontend/src/pages/QuizPage.jsx
+++ quiz-app-frontend/src/pages/QuizPage.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import "../assets/styles/QuizPage.css";
-import questions from "../components/Question"
+import questionsData from "../components/Question"
+
+const shuffleArray = (array) => {
+    for(let i= array.length -1; i > 0; i--){
+        const j = Math.floor(Math.random()*(i+1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
 
 const QuizPage = () => 
     { 
         const location = useLocation();
         const name = location.state.name;
+        const [ questions , setQuestions] = useState([]);
         const [ currentQuestionIndex, setCurrentQuestionIndex  ] = useState(0);
-        const [ selectedOptions, setSelectedOptions ] = useState(Array(questions.length).fill(null));
+        const [ selectedOptions, setSelectedOptions ] = useState([]);
+        const [isQuizCompleted, setIsQuizCompleted] = useState(false);
+
+        useEffect(() => {
+            // Shuffle questions and options
+            const shuffledQuestions = shuffleArray([...questionsData]).map((questionData) => ({
+                ...questionData,
+                options: shuffleArray([...questionData.options]),
+            }));
+            setQuestions(shuffledQuestions);
+            setSelectedOptions(Array(shuffledQuestions.length).fill(null));
+        },[]);
         const handleQuestionClick = (index) => {
             setCurrentQuestionIndex(index);
         }
@@ -19,8 +39,17 @@ const QuizPage = () =>
         }
         const handleSubmit = () => {
             console.log("Selected options:", selectedOptions);
-            
+            setIsQuizCompleted(true);
             // You can send the selectedOptions to a server or process them as needed
+        }
+        if (isQuizCompleted) {
+            return (
+                <div className="quiz-page">
+                    <h1>Quiz Completed</h1>
+                    <h2>Results:</h2>
+                      
+                </div>
+            );
         }
         return(
         <div className="quiz-page">
@@ -37,6 +66,7 @@ const QuizPage = () =>
             </div>
             <div className="quiz-container" >
                 <h1>Welcome,  {name}</h1>
+                {questions.length > 0 &&(
                 <div className="question">
                     <h2>{questions[currentQuestionIndex].question}</h2>
                     <div className="options">
@@ -50,7 +80,7 @@ const QuizPage = () =>
                             </div>
                         ))}
                     </div>
-                </div>
+                </div>)}
                 {currentQuestionIndex == questions.length - 1 && (
                     <button className="submit-button" onClick={handleSubmit}>
                         Submit
